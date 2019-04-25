68059259c62b5dc492cb63e19b45a53f846d4f3b
Tom S <thoschulte@gmail.com>
2019-04-25 11:51:47 +0200

    caching

--- server.js
+++ server.js
@@ -4,39 +4,39 @@ const numCPUs = require('os').cpus().length;
 const R = require('ramda');
 
 if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
+  console.log(`Master ${process.pid} is running`);
 
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
 } else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    http.createServer((req, res) => {
-        res.writeHead(200);
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  http.createServer((req, res) => {
+    res.writeHead(200);
 
-        res.end(addString('hello', ' ', 'world', ' ', '...', '\n'));
-    }).listen(8000);
+    res.end(addString('hello', ' ', 'world', ' ', '...', '\n'));
+  }).listen(8000);
 
-    /**
-     * This function ...
-     * @param {string} input any strings
-     * @returns {string} joined strings
-     */
-    function addString(...input) {
-        let returnValue = '';
+  /**
+  * This function ...
+  * @param {string} input any strings
+  * @return {string} joined strings
+  */
+  function addString(...input) {
+    let returnValue = '';
 
-        R.forEach(element => {
-            returnValue = R.concat(returnValue, element);
-        }, input);
+    R.forEach((element) => returnValue = R.concat(returnValue, element), input);
 
-        return returnValue;
-    }
+    // input.forEach((element) => returnValue = R.concat(returnValue, element));
 
-    console.log(`Worker ${process.pid} started...`);
+    return returnValue;
+  }
+
+  console.log(`Worker ${process.pid} started...`);
 }
